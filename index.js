3760c380c81a710a107053e46b3a41870b6dc68d
brent-edwards-online <brent@brentedwardsonline.com>
2017-07-15 12:59:32 +1000

    Node micro

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { send } = require('micro')
 const url = require('url')
+const level = require('level')
+const promisify = require('then-levelup')
 
-module.exports = function (request, response) {
-  const { pathname } = url.parse(request.url);
-  console.log(pathname);
-  send(response, 200, pathname);
+const db = promisify(level('visits.db', {
+  valueEncoding: 'json'
+}))
+
+module.exports = async function (request, response) {
+  const { pathname } = url.parse(request.url)
+
+  try {
+    const currentVisits = await db.get(pathname)
+    await db.put(pathname, currentVisits + 1)
+  } catch (error) {
+    if (error.notFound) await db.put(pathname, 1)
+  }
+
+  send(response, 200, `This page has ${await db.get(pathname)} visits!`)
 }
